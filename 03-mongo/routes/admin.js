896973b243928d32d3daa6ceb330a8383a0df4e4
deepakcode21@gmail.com <143344721+ArrowCod@users.noreply.github.com>
2024-12-10 19:40:27 +0530

    mongoDb assigment done

--- 03-mongo/routes/admin.js
+++ 03-mongo/routes/admin.js
@@ -1,18 +1,49 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await Admin.create({
+        username: username,
+        password: password
+    })
+    res.json({
+        message: "Admin Created Successfully"
+    })
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+    // zod 
+   const newCourse = await Course.create({
+        title, 
+        description,
+        imageLink,
+        price
+    })
+    console.log(newCourse)
+    res.json({
+        message: 'Course created successfully', courseId: newCourse._id
+    })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
     // Implement fetching all courses logic
+    Course.find({}).then(function(response){
+        res.json({
+            courses: response
+        })
+    })
 });
 
 module.exports = router;
