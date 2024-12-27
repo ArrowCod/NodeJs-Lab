3c8033f31c406238fb2e78340df7c591d02db053
deepakcode21@gmail.com <143344721+ArrowCod@users.noreply.github.com>
2024-12-28 01:18:42 +0530

    cardWapper learn

--- react-learn/class-A/src/App.jsx
+++ react-learn/class-A/src/App.jsx
@@ -1,51 +1,23 @@
 import React from "react";
 import { useState } from "react";
 
-let counter = 4; 
+
 function App() {
 
-  const [todos, setTodos] = useState([{
-    id: 1,
-    title: "gym",
-    description: "go to gym today"
-  },{
-    id: 2,
-    title: "college",
-    description: "go to college today"
-  },{
-    id: 3,
-    title: "Reading",
-    description: "complete the book today!"
-  }])
-
-  function addTodo(){
-    setTodos([...todos, {
-      id: counter++,
-      title: Math.random(),
-      description: Math.random()
-    }])
-  }
- 
   return (
     <div>
-      <button onClick={addTodo} >Add Todo</button>
-     {todos.map(todos => <Todo key={todos.id} title={todos.title} description={todos.description} />)}
-    </div>
-  )
-}
-function Todo({title, description}){
-
-  return(
-    <div>
-      <h1>
-        {title}
-      </h1>
-      <h3>
-        {description}
-      </h3>
+      <CardWrapper>  
+        hi there  
+      </CardWrapper>     
     </div>
   )
 }
 
+function CardWrapper({children}){
+  return <div style={{border: "2px solid black", padding: 20, margin: 20}}>
+    {children}
+  </div>
+  
+}
 
 export default App
